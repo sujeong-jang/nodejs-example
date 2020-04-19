5566a75262185253ec8fbc512a1b408a88394a3e
Sujeong Jang <56670564+sujeong-jang@users.noreply.github.com>
2020-04-19 15:48:09 +0900

    ejs를 활용한 응답처리

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 var express = require('express')
-
+var bodyParser = require('body-parser')
 var app = express()
+
 app.listen(3000, function() {
   console.log("start! express server on port 3000");
 });
 
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+
+//url routing
 app.get('/', function(req, res) {
   res.sendFile(__dirname + "/public/main.html");
+});
+
+app.get('/main', function(req, res) {
+  res.sendFile(__dirname + "/public/main.html");
+});
+
+app.post('/email_post', function(req, res){
+  //get:req.param('email')
+  //npm install body-parser
+  console.log(req.body.email);
+  res.render('email.ejs', {'email' : req.body.email});
 });
